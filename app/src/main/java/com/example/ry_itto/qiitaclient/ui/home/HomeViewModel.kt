4b30fe9c3f24e5e2e90a3939d5eaa21a0043fcb7
ryoya ito <horusu.dqx@gmail.com>
2019-10-29 03:21:29 +0900

    recyclerViewでの見た目を改善

--- app/src/main/java/com/example/ry_itto/qiitaclient/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ry_itto/qiitaclient/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.ry_itto.qiitaclient.ui.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.ry_itto.qiitaclient.domain.QiitaAPIClient
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -10,4 +11,7 @@ class HomeViewModel : ViewModel() {
     val articles = QiitaAPIClient.fetchArticles("android")
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
+        .doOnError {
+            Log.w("articles", it.toString())
+        }
 }
